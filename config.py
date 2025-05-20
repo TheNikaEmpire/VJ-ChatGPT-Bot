64f7484fca40b34301839eb4469c2d86a790c809
TheNikaEmpire <thenikaempire@gmail.com>
2025-05-20 11:38:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ def is_enabled(value, default):
     else:
         return default
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "29962141"))
+API_HASH = environ.get("API_HASH", "27fcf227b58f85cedf46098b9c2ea240")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
-DB_URI = environ.get("DB_URI", "")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002570967954"))
+ADMINS = int(environ.get("ADMINS", "7483283743"))
+DB_URI = environ.get("DB_URI", "mongodb+srv://thenikaempire:BMiN7Qou9Y94zau7@cluster0.00jz2ru.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "chatgptvjbot")
 OPENAI_API = environ.get("OPENAI_API", "")
 AI = is_enabled((environ.get("AI","True")), False)
